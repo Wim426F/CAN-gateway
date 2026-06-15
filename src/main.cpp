8d08aace0a9ff0914e2b4980ca628d8f518f42bf
Wim426F <wim.boone@outlook.com>
2026-06-15 19:40:38 +0200

    Pass all BMW to VCU messages through

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include <Snooze.h>
 
 // CAN settings
-FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can1; // BMW-E90 side
-FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can2; // VCU side
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_64> Can1; // BMW-E90 side
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_64> Can2; // VCU side
 CAN_message_t rxMsg, txMsg;
 
 // Stored latest 0x1A0 message from Can1
@@ -28,31 +28,31 @@ const int CAN1_RX_PIN = 23; // Default for Teensy 4.1 CAN1 RX (adjust if differe
 
 void initCanFilters() {
   // Can1 filters (BMW side)
-  Can1.setMBFilter(MB0, 0x130); // E90 CAS
-  Can1.setMBFilter(MB1, 0x2FC); // E90 Enclosure status
-  Can1.setMBFilter(MB2, 0x480); // Network Management
-  Can1.setMBFilter(MB3, 0x1A0); // Speed
-  Can1.setMBFilter(MB4, 0x194); // Cruise control
-  Can1.setMBFilter(MB5, 0x19E); // Brake DTC status
-  Can1.setMBFilter(MB6, 0x0C4); // Steering wheel angle
-  Can1.setMBFilter(MB7, 0x0CE); // Wheel speeds
-  Can1.setMBFilter(MB8, 0x0C8); // Steering wheel angle slow
-  Can1.setMBFilter(MB9, 0x0B6); // Dynamic cruise controle torque demand
-  Can1.setMBFilter(MB10, 0x5A9); // Services DSC
-  Can1.setMBFilter(MB11, 0x1B4); // Vehicle speed
-  Can1.setMBFilter(MB12, 0x1A1); // Vehicle speed
-  Can1.setMBFilter(MB13, 0x1A6); // Vehicle speed for instrument cluster
-  Can1.setMBFilter(MB14, 0x1D6); // Steering wheel buttons
-  Can1.setMBFilter(MB15, 0x26E); // Ignition status
-  Can1.setMBFilter(MB16, 0x2CA); // Outside Temperature
-  Can1.setMBFilter(MB17, 0x2D6); // AC compressor on/off request 
-  Can1.setMBFilter(MB18, 0x2E6); // Climate control settings
-  Can1.setMBFilter(MB19, 0x2EA); // Climate control settings passenger
-  Can1.setMBFilter(MB20, 0x242); // Climate front status
-  Can1.setMBFilter(MB21, 0x200); // Cruise control status
-  Can1.setMBFilter(MB22, 0x1A0); // Vehicle speed for EPS
-  Can1.setMBFilter(MB23, 0x330); // Vehicle speed for EPS
-  Can1.setMBFilter(MB24, 0x310); // Ambient temp and relative time
+  //Can1.setMBFilter(MB0, 0x130); // E90 CAS
+  //Can1.setMBFilter(MB1, 0x2FC); // E90 Enclosure status
+  //Can1.setMBFilter(MB2, 0x480); // Network Management
+  //Can1.setMBFilter(MB3, 0x1A0); // Speed
+  //Can1.setMBFilter(MB4, 0x194); // Cruise control
+  //Can1.setMBFilter(MB5, 0x19E); // Brake DTC status
+  //Can1.setMBFilter(MB6, 0x0C4); // Steering wheel angle
+  //Can1.setMBFilter(MB7, 0x0CE); // Wheel speeds
+  //Can1.setMBFilter(MB8, 0x0C8); // Steering wheel angle slow
+  //Can1.setMBFilter(MB9, 0x0B6); // Dynamic cruise controle torque demand
+  //Can1.setMBFilter(MB10, 0x5A9); // Services DSC
+  //Can1.setMBFilter(MB11, 0x1B4); // Vehicle speed
+  //Can1.setMBFilter(MB12, 0x1A1); // Vehicle speed
+  //Can1.setMBFilter(MB13, 0x1A6); // Vehicle speed for instrument cluster
+  //Can1.setMBFilter(MB14, 0x1D6); // Steering wheel buttons
+  //Can1.setMBFilter(MB15, 0x26E); // Ignition status
+  //Can1.setMBFilter(MB16, 0x2CA); // Outside Temperature
+  //Can1.setMBFilter(MB17, 0x2D6); // AC compressor on/off request 
+  //Can1.setMBFilter(MB18, 0x2E6); // Climate control settings
+  //Can1.setMBFilter(MB19, 0x2EA); // Climate control settings passenger
+  //Can1.setMBFilter(MB20, 0x242); // Climate front status
+  //Can1.setMBFilter(MB21, 0x200); // Cruise control status
+  //Can1.setMBFilter(MB22, 0x1A0); // Vehicle speed for EPS
+  //Can1.setMBFilter(MB23, 0x330); // Vehicle speed for EPS
+  //Can1.setMBFilter(MB24, 0x310); // Ambient temp and relative time
 
   // Can2 filters (VCU side)
   Can2.setMBFilter(MB0,  0x0AA); // ABS/DSC info
@@ -64,7 +64,7 @@ void initCanFilters() {
   Can2.setMBFilter(MB6,  0x1D2); // Shift position
   Can2.setMBFilter(MB7,  0x592); // Error lights
   Can2.setMBFilter(MB8,  0x200); // Cruise control status
-  Can2.setMBFilter(MB13,  0x1B5); // Heat flow climate
+  Can2.setMBFilter(MB14,  0x1B5); // Heat flow climate
   Can2.setMBFilter(MB9,  0x1B6); // Heat flow engine
   Can2.setMBFilter(MB10, 0x381); // Engine oil level
   Can2.setMBFilter(MB11, 0x3B3); // Power management consumption
